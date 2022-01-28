77296366a2d6800c04f9e47a59bf29809b9dff2a
nsagark <90008930+nsagark@users.noreply.github.com>
2022-01-28 13:33:11 -0500

    Update remove-users-notpartofteams.sh

--- remove-users-notpartofteams.sh
+++ remove-users-notpartofteams.sh
@@ -2,7 +2,7 @@
 
 #set -x
 
-
+# This script is used to delete nirmata users who do not belong to any teams. Before you run this script, please make sure you have curl and jq installed. Make sure to update the <api-key> value from the nirmata profile. 
 
 TOKEN=<api-key>
 URL=www.nirmata.io
