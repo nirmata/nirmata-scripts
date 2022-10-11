5593365c4e8b7b469e349e8f61dbb3bb1f4f50de
nsagark <90008930+nsagark@users.noreply.github.com>
2022-10-10 23:21:38 -0400

    Create find-licenses.sh

---
+++ licenses_used/find-licenses.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/bash
+
+NIRMATAURL="https://nirmata.io"
+TOTAL_LICENSES=0
+
+echo
+echo -e "Enter the Nirmata API token: \n"
+read -s TOKEN
+
+curl -s -H "Accept: application/json, text/javascript, */*; q=0.01" -H "Authorization: NIRMATA-API $TOKEN" -X GET "$NIRMATAURL/cluster/api/KubernetesCluster?fields=name" | jq .[].name | sed "s/\"//g" > clusters.txt
+
+echo
+python -c 'print "-" * 120'
+printf '%-40s %-15s %-15s %-15s %-15s %-15s\n' CLUSTERNAME MEMORY_LIMIT MEMORY_REQUEST MEMORY_CAPACITY MEMORY_USAGE LICENSES_USED
+python -c 'print "-" * 120'
+
+for CLUSTERNAME in $(cat clusters.txt)
+do
+        CLUSTERID=$(curl -s -H "Accept: application/json, text/javascript, */*; q=0.01" -H "Authorization: NIRMATA-API $TOKEN" -X GET "$NIRMATAURL/cluster/api/KubernetesCluster?fields=id,name" 2>&1 | jq ".[] | select( .name == \"$CLUSTERNAME\" ).id" | sed "s/\"//g")
+        #echo -e "$CLUSTERNAME:\t$CLUSTERID"
+        MEMCAP=$(curl -s -H "Accept: application/json, text/javascript, */*; q=0.01" -H "Authorization: NIRMATA-API $TOKEN" -X GET "$NIRMATAURL/cluster/api/KubernetesCluster/$CLUSTERID/ClusterStats?fields=memoryCapacity" | jq .[].memoryCapacity)
+        MEMLIMIT=$(curl -s -H "Accept: application/json, text/javascript, */*; q=0.01" -H "Authorization: NIRMATA-API $TOKEN" -X GET "https://nirmata.io/cluster/api/KubernetesCluster/$CLUSTERID/ClusterStats?fields=memoryLimit" | jq .[].memoryLimit)
+        MEMREQUEST=$(curl -s -H "Accept: application/json, text/javascript, */*; q=0.01" -H "Authorization: NIRMATA-API $TOKEN" -X GET "https://nirmata.io/cluster/api/KubernetesCluster/$CLUSTERID/ClusterStats?fields=memoryRequest" | jq .[].memoryRequest)
+        MEMUSAGE=$(curl -s -H "Accept: application/json, text/javascript, */*; q=0.01" -H "Authorization: NIRMATA-API $TOKEN" -X GET "https://nirmata.io/cluster/api/KubernetesCluster/$CLUSTERID/ClusterStats?fields=memoryUsage" | jq .[].memoryUsage)
+        MEMCAP_GiB=$(jq -n $MEMCAP/1073741824)
+        MEMLIMIT_GiB=$(jq -n $MEMLIMIT/1073741824)
+        MEMREQUEST_GiB=$(jq -n $MEMREQUEST/1073741824)
+        MEMUSAGE_GiB=$(jq -n $MEMUSAGE/1073741824)
+        MEMCAP_ROUND=$(echo $MEMCAP_GiB | awk '{print int($1+0.5)}')
+        (( LICENSES_USED=($MEMCAP_ROUND+32-1)/32 ));
+        let TOTAL_LICENSES=$((TOTAL_LICENSES + LICENSES_USED))
+        printf '%-40s %-15.2f %-15.2f %-15s %-15.2f %-15s\n' $CLUSTERNAME $MEMLIMIT_GiB $MEMREQUEST_GiB $MEMCAP_ROUND $MEMUSAGE_GiB $LICENSES_USED
+
+done
+
+python -c 'print "-" * 120'
+printf '%104s %-15s\n' TOTAL_LICENSES: $TOTAL_LICENSES
+echo -e "\nNOTE: All the memory values defined in the table above are in GiB\n"
