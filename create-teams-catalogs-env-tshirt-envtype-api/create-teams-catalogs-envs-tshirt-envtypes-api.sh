68eb68addc9c30aa2291353900c82b894f6da146
nsagark <90008930+nsagark@users.noreply.github.com>
2022-10-31 20:40:10 -0400

    Update create-teams-catalogs-envs-tshirt-envtypes-api.sh

--- create-teams-catalogs-env-tshirt-envtype-api/create-teams-catalogs-envs-tshirt-envtypes-api.sh
+++ create-teams-catalogs-env-tshirt-envtype-api/create-teams-catalogs-envs-tshirt-envtypes-api.sh
@@ -41,7 +41,7 @@ do
           \"delete\": []
         }"
         if [[ $? = 0 ]]; then
-                echo "Team \"$team-$STRING-team\" created successfully"
+                echO "Team \"$team-$STRING-team\" created successfully"
         else
                 echo "Something went wrong when creating team \"$team-$STRING-team\""
         fi
@@ -93,7 +93,7 @@ do
           \"delete\": []
         }"
         if [[ $? = 0 ]]; then
-                echo "Catalog \"$catalog-$STRING-catalog\" created successfully"
+                echO "Catalog \"$catalog-$STRING-catalog\" created successfully"
         else
                 echo "Something went wrong when creating catalog \"$catalog-$STRING-catalog\""
         fi
@@ -105,13 +105,18 @@ do
         #set -x
         CLUSTERID=$(curl -s -H "Accept: application/json, text/javascript, */*; q=0.01" -H "Authorization: NIRMATA-API $TOKEN" -X GET "$NIRMATAURL/cluster/api/KubernetesCluster?fields=id,name" | jq ".[] | select( .name == \"$CLUSTERNAME\" ).id" | sed "s/\"//g")
 
-        ENVIRONMENT_TYPE=""
+        ENVIRONMENT_TYPE_EXISTS=""
         ENVIRONMENT_TYPE=$(echo $environment | cut -d "," -f 2)
         environment=$(echo $environment | cut -d "," -f 1)
         ENVIRONMENT_EXISTS=""
         ENVIRONMENT_EXISTS=$(curl -s -H "Accept: application/json, text/javascript, */*; q=0.01" -H "Authorization: NIRMATA-API $TOKEN" -X GET "$NIRMATAURL/environments/api/environments?fields=id,name" | jq ".[] | select( .name == \"$environment-$STRING\" ).id" | sed "s/\"//g")
         if [[ ! -z $ENVIRONMENT_EXISTS ]]; then
                 echo "Environment \"$environment-$STRING\" already exists"
+                continue
+        fi
+        ENVIRONMENT_TYPE_EXISTS=$(curl -s -H "Accept: application/json, text/javascript, */*; q=0.01" -H "Authorization: NIRMATA-API $TOKEN" -X GET "$NIRMATAURL/environments/api/EnvironmentResourceType?fields=id,name" | jq ".[] | select( .name == \"$ENVIRONMENT_TYPE\" ).id" | sed "s/\"//g")
+        if [[ -z $ENVIRONMENT_TYPE_EXISTS ]]; then
+                echo "Environment \"$environment-$STRING\" cannot be created as \"$ENVIRONMENT_TYPE\" does not exist"
                 continue
         fi
         OWNER_ID=$(curl -s -H "Accept: application/json, text/javascript, */*; q=0.01" -H "Authorization: NIRMATA-API $TOKEN" -X GET "$NIRMATAURL/users/api/teams?fields=id,name" | jq ".[] | select( .name == \"$environment-$STRING-team\" ).id" | sed "s/\"//g")
@@ -155,7 +160,7 @@ do
           \"delete\": []
         }"
         if [[ $? = 0 ]]; then
-                echo "Environment \"$environment-$STRING\" created successfully"
+                echO "Environment \"$environment-$STRING\" created successfully"
         else
                 echo "Something went wrong when creating environment \"$environment-$STRING\""
         fi
