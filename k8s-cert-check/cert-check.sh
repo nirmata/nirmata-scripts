77d2e8d3769d6a0a0b1e5b0eb608b9ab24f7091d
nirmata <nirmata@Anudeeps-MacBook-Pro.local>
2023-01-06 00:11:21 +0530

    Added script for k8s cert check

---
+++ k8s-cert-check/cert-check.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+FILE_DATE=$(date -r /opt/nirmata/ssl/kubelet.crt "+%Y-%m-%d")
+CURRENT_DATE=$(date "+%Y-%m-%d")
+DATE_DIFF=$(( ($(date -d "$CURRENT_DATE" +%s) - $(date -d "$FILE_DATE" +%s)) / (60*60*24) ))
+echo "The number of days when the certificate is created: $DATE_DIFF"
+YEAR=365
+CERT_DIFF=$DATE_DIFF
+#Number of days left to expire
+let "DAYS_LEFT=$YEAR-$CERT_DIFF"
+echo "Number of days left to expire: $DAYS_LEFT"
+# Number of days in the warning threshhold
+WARNDAYS=30
+if [ "${DAYS_LEFT}" -le "${WARNDAYS}" ]; then
+   #     echo "The cert will expire soon!"
+        echo '"Hi, k8s certs is going to expire soon on zephyr production."' | mail -r "Zephyr-prod" -s "k8s certs will be expire soon : Zephyr Prod" "anudeep.nalla@nirmata.com" # Action if true
+else
+        echo "Looks good till now!"
+fi
