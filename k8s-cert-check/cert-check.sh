9fb602b6c4df3e50747cd6cf0c5703dc00ec5df7
anudeep-nirmata <121174814+anuddeeph1@users.noreply.github.com>
2023-02-09 11:40:38 +0530

    Update cert-check.sh
    Added cluster name and environment as variable

--- k8s-cert-check/cert-check.sh
+++ k8s-cert-check/cert-check.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+CLUSTER_NAME=<cluster-name>  #Enter cluster name
+ENVIRONMENT=<Environment>    #sandbox or dev or Production
 FILE_DATE=$(date -r /opt/nirmata/ssl/kubelet.crt "+%Y-%m-%d")
 CURRENT_DATE=$(date "+%Y-%m-%d")
 DATE_DIFF=$(( ($(date -d "$CURRENT_DATE" +%s) - $(date -d "$FILE_DATE" +%s)) / (60*60*24) ))
@@ -9,10 +11,10 @@ CERT_DIFF=$DATE_DIFF
 let "DAYS_LEFT=$YEAR-$CERT_DIFF"
 echo "Number of days left to expire: $DAYS_LEFT"
 # Number of days in the warning threshhold
-WARNDAYS=30
+WARNDAYS=365
 if [ "${DAYS_LEFT}" -le "${WARNDAYS}" ]; then
    #     echo "The cert will expire soon!"
-        echo '"Hi, k8s certs is going to expire soon on production."' | mail -r "prod" -s "k8s certs will be expire soon :  Prod" "<Receiver-Email>" # Action if true
+        echo '"Hi, k8s certs is going to expire soon on cluster named '$CLUSTER_NAME' in '$ENVIRONMENT' Environment"' | mail -r "Nirmata" -s "k8s certs will be expire soon :  '$ENVIRONMENT'" "<Receiver-Email>" # Action if true
 else
         echo "Looks good till now!"
 fi
