4af87a7d2bc5192c806e09460e6991f9bdecc3ac
nsagark <90008930+nsagark@users.noreply.github.com>
2023-04-06 07:43:05 -0400

    Update update-kubelet-args.sh

--- update-kubelet-args/update-kubelet-args.sh
+++ update-kubelet-args/update-kubelet-args.sh
@@ -1,5 +1,52 @@
 #!/usr/bin/bash
 
+## function to check and install jq based on the ostype
+
+installjq() {
+
+        # Check the operating system
+        if [[ "$(uname)" == "Darwin" ]]; then
+                # Mac OS X
+                brew install jq
+        elif [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
+                # Linux
+                if [[ -n "$(command -v yum)" ]]; then
+                        # CentOS, RHEL, Fedora
+                        sudo yum install -y jq
+                elif [[ -n "$(command -v apt-get)" ]]; then
+                        # Debian, Ubuntu, Mint
+                        sudo apt-get update
+                        sudo apt-get install -y jq
+                elif [[ -n "$(command -v zypper)" ]]; then
+                        # OpenSUSE
+                        sudo zypper install -y jq
+                elif [[ -n "$(command -v pacman)" ]]; then
+                        # Arch Linux
+                        sudo pacman -S --noconfirm jq
+                else
+                        echo "Error: Unsupported Linux distribution."
+                        exit 1
+                fi
+        else
+                echo "Error: Unsupported operating system."
+                exit 1
+        fi
+
+        # Print the version of jq installed
+        jq --version
+
+}
+
+## main
+
+if [[ -n "$(command -v jq)" ]]; then
+    echo "jq is installed."
+    jq --version
+else
+    echo -e "\njq is not installed. Installing jq ...\n"
+    installjq
+fi
+
 if [[ $# != 3 ]]; then
         echo -e "\nUsage: \t$0 <cluster-name> <Nirmata URL> <Kubelet-arg-to-add>\n"
         echo -e "Example: $0 demo-cluster https://nirmata.io --cgroup-driver=systemd"
