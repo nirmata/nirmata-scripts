b738106792805121ef1cdd297dd3f466e02bce27
Vikash Kaushik <111422528+vikashkaushik01@users.noreply.github.com>
2024-01-12 18:42:17 +0530

    Update ansible-prerequisites.ps1

--- windows-k8s/ansible-prerequisites.ps1
+++ windows-k8s/ansible-prerequisites.ps1
@@ -45,7 +45,7 @@ function VerifyPowerShell(){
 }
 function InstallWINRM (){
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    $url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
+    $url = "https://raw.githubusercontent.com/ansible/ansible-documentation/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
     $file = "$env:temp\ConfigureRemotingForAnsible.ps1" 
     (New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
     powershell.exe -ExecutionPolicy ByPass -File $file
